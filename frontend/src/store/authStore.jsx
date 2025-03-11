47afe92988d4f96e0d30cbb801dc16292601f335
seongwong woo <dnqkr18@gmail.com>
2025-03-11 19:43:27 +0900

    fix: 로그인 상태관리 로컬호스트로 변경

--- frontend/src/store/authStore.jsx
+++ frontend/src/store/authStore.jsx
@@ -1,9 +1,15 @@
 import { create } from 'zustand';
 
 const useAuthStore = create((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-  clearUser: () => set({ user: null }),
+  user: JSON.parse(localStorage.getItem('user')) || null,
+  setUser: (user) => {
+    localStorage.setItem('user', JSON.stringify(user));
+    set({ user });
+  },
+  clearUser: () => {
+    localStorage.removeItem('user');
+    set({ user: null });
+  },
 }));
 
 export default useAuthStore;
